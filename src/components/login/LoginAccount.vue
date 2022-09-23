2987197f32080f6a12f077cbf3c13146708e96c6
citcen <citkecen@gmail.com>
2022-09-23 16:00:11 +0800

    feat: account login

--- src/components/login/LoginAccount.vue
+++ src/components/login/LoginAccount.vue
@@ -1,34 +1,62 @@
 <template>
   <el-form
-    :model="ruleForm"
     ref="formRef"
+    :model="accountForm"
     :rules="accountRules"
     label-width="70px"
   >
     <el-form-item label="账号" prop="name">
-      <el-input v-model="ruleForm.name" />
+      <el-input v-model="accountForm.name" />
     </el-form-item>
     <el-form-item label="密码" prop="password">
-      <el-input v-model="ruleForm.password" show-password />
+      <el-input v-model="accountForm.password" show-password />
     </el-form-item>
   </el-form>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from 'vue'
+import { defineComponent, reactive, ref } from 'vue'
+import { useStore } from 'vuex'
 import type { LoginAccount } from '@/types/login'
 import { accountRules } from '@/components/config/rules'
+import type { FormInstance } from 'element-plus'
+import localCache from '@/utils/cache'
 
 export default defineComponent({
-  setup() {
-    const ruleForm = reactive({
-      name: '',
-      password: ''
+  setup: function () {
+    const store = useStore()
+
+    const accountForm = reactive({
+      name: localCache.getCache('_name_') ?? '',
+      password: localCache.getCache('_pwd_') ?? ''
     })
+    const formRef = ref<FormInstance>()
+
+    const accountLogin = (isKeepPwd: boolean) => {
+      // 验证用户名和密码
+      formRef.value?.validate((valid) => {
+        if (valid) {
+          const name = accountForm.name
+          const pwd = accountForm.password
+          // 是否保存密码
+          if (isKeepPwd) {
+            localCache.setCache('_name_', name)
+            localCache.setCache('_pwd_', pwd)
+          } else {
+            localCache.delCache('_name_')
+            localCache.delCache('_pwd_')
+          }
+
+          store.dispatch('loginModule/accountLoginAction', { ...accountForm })
+        }
+      })
+    }
 
     return {
-      ruleForm,
-      accountRules
+      formRef,
+      accountForm,
+      accountRules,
+      accountLogin
     }
   }
 })
