7ba3fe6fbbb4220d57247308a303988742e21fdd
citcen <citkecen@gmail.com>
2022-09-23 15:59:08 +0800

    feat: login page

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,13 +1,31 @@
 <template>
-  <div>Login</div>
+  <div class="login">
+    <login-panel />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import LoginPanel from '@/components/login/LoginPanel.vue'
 
 export default defineComponent({
+  components: {
+    LoginPanel
+  },
   setup() {
     return {}
   }
 })
 </script>
+
+<style scoped>
+.login {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background: url('@/assets/images/login_bg.jpg');
+  background-size: 100%;
+}
+</style>
