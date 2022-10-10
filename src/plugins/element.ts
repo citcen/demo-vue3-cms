cd2a4d740a9334a66905fe0c91ea9b5e4a433ce9
citcen <citkecen@gmail.com>
2022-10-10 16:30:57 +0800

    feat: add element cmp

--- src/plugins/element.ts
+++ src/plugins/element.ts
@@ -1,30 +1,94 @@
 import type { App } from 'vue'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import 'element-plus/dist/index.css'
 import {
   ElButton,
-  ElTable,
-  ElAlert,
+  ElTabs,
+  ElTabPane,
+  ElForm,
+  ElFormItem,
+  ElInput,
+  ElCheckbox,
+  ElLink,
+  ElMenu,
+  ElMenuItem,
+  ElContainer,
   ElAside,
-  ElAutocomplete,
+  ElHeader,
+  ElMain,
+  ElDropdown,
+  ElDropdownMenu,
+  ElDropdownItem,
   ElAvatar,
-  ElBacktop,
-  ElBadge
+  ElButtonGroup,
+  ElCol,
+  ElRow,
+  ElSelect,
+  ElOption,
+  ElDatePicker,
+  ElBreadcrumb,
+  ElBreadcrumbItem,
+  ElTable,
+  ElTableColumn,
+  ElPagination,
+  ElConfigProvider,
+  ElDialog,
+  ElImage,
+  ElTree,
+  ElDescriptions,
+  ElDescriptionsItem,
+  ElTag,
+  ElCard,
+  ElTooltip
 } from 'element-plus'
 
 const components = [
   ElButton,
-  ElTable,
-  ElAlert,
+  ElTabs,
+  ElTabPane,
+  ElForm,
+  ElFormItem,
+  ElInput,
+  ElCheckbox,
+  ElLink,
+  ElMenu,
+  ElMenuItem,
+  ElContainer,
   ElAside,
-  ElAutocomplete,
+  ElHeader,
+  ElMain,
+  ElDropdown,
+  ElDropdownMenu,
+  ElDropdownItem,
   ElAvatar,
-  ElBacktop,
-  ElBadge
+  ElButtonGroup,
+  ElRow,
+  ElCol,
+  ElSelect,
+  ElOption,
+  ElDatePicker,
+  ElBreadcrumb,
+  ElBreadcrumbItem,
+  ElTable,
+  ElTableColumn,
+  ElPagination,
+  ElConfigProvider,
+  ElDialog,
+  ElImage,
+  ElTree,
+  ElDescriptions,
+  ElDescriptionsItem,
+  ElTag,
+  ElCard,
+  ElTooltip
 ]
 
 // 全局引用常用element-plus 组件
 export default (app: App) => {
   for (const cpn of components) {
     app.component(cpn.name, cpn)
+  }
+  for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component as any)
   }
 }
