fc5b83be239bfb180e5e4db9da057d93317059b8
citcen <citkecen@gmail.com>
2022-10-10 16:33:20 +0800

    feat: login

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
-  <h2>{{ $store.state.message }}</h2>
-  <RouterLink to="/">main</RouterLink>
-  <RouterLink to="/login">
-    <el-button type="primary">登录</el-button>
-  </RouterLink>
+  <div class="app"></div>
   <RouterView />
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { RouterLink, RouterView } from 'vue-router'
+
+export default defineComponent({
+  components: {
+    RouterLink,
+    RouterView
+  },
+  setup() {
+    return {}
+  }
+})
+</script>
 
 <style scoped></style>
