f48f7d220bcf50f584053dd9fe0546a5e999c3a5
citcen <citkecen@gmail.com>
2022-10-12 19:59:32 +0800

    feat: use IUserStore get menus

--- src/components/navMenu/scr/NavMenu.vue
+++ src/components/navMenu/scr/NavMenu.vue
@@ -4,15 +4,53 @@
       <img src="~@/assets/images/logo.svg" alt="logo" />
       <span>vue3</span>
     </div>
+    <el-menu default-active="2" class="el-menu-vertical">
+      <template v-for="item in userMenus" :key="item.id"></template>
+      <!--      <el-sub-menu index="1">
+        <template #title>
+          <el-icon><location /></el-icon>
+          <span>Navigator One</span>
+        </template>
+        <el-menu-item-group title="Group One">
+          <el-menu-item index="1-1">item one</el-menu-item>
+          <el-menu-item index="1-2">item two</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="Group Two">
+          <el-menu-item index="1-3">item three</el-menu-item>
+        </el-menu-item-group>
+        <el-sub-menu index="1-4">
+          <template #title>item four</template>
+          <el-menu-item index="1-4-1">item one</el-menu-item>
+        </el-sub-menu>
+      </el-sub-menu>
+      <el-menu-item index="2">
+        <el-icon><icon-menu /></el-icon>
+        <span>Navigator Two</span>
+      </el-menu-item>
+      <el-menu-item index="3" disabled>
+        <el-icon><document /></el-icon>
+        <span>Navigator Three</span>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <el-icon><setting /></el-icon>
+        <span>Navigator Four</span>
+      </el-menu-item>-->
+    </el-menu>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, computed } from 'vue'
+import { IUseStore } from '@/store'
 
 export default defineComponent({
   setup() {
-    return {}
+    const store = IUseStore()
+    const userMenus = computed(() => store.state.login.menus)
+
+    return {
+      userMenus
+    }
   }
 })
 </script>
