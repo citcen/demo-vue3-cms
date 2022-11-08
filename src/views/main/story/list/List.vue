1c96a2ee13ad7a5260d6bcc380f1443bc8a71453
citcen <citkecen@gmail.com>
2022-11-08 16:56:29 +0800

    feat: init page

---
+++ src/views/main/story/list/List.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>List</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    return {}
+  }
+})
+</script>
