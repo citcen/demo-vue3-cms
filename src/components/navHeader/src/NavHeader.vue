181c4d74564e6558e3a11fa2102ac3f89ad086b1
citcen <citkecen@gmail.com>
2022-11-11 14:48:35 +0800

    feat: navHeader info

--- src/components/navHeader/src/NavHeader.vue
+++ src/components/navHeader/src/NavHeader.vue
@@ -4,13 +4,24 @@
       <Expand v-if="isFold" />
       <Fold v-else />
     </el-icon>
+    <div class="content">
+      <div class="breadcrumb">
+        <span>第一个</span>
+        <span>第二个</span>
+      </div>
+      <UserInfo />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
+import UserInfo from './UserInfo.vue'
 
 export default defineComponent({
+  components: {
+    UserInfo
+  },
   emits: ['foldChange'],
   setup(props, { emit }) {
     const isFold = ref(false)
@@ -27,9 +38,20 @@ export default defineComponent({
 </script>
 <style scoped lang="less">
 .nav-header {
+  display: flex;
+  width: 100%;
+
   .el-icon {
     font-size: 30px;
     cursor: pointer;
+  }
+
+  .content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex: 1;
+    padding: 0 20px;
   }
 }
 </style>
---
+++ src/components/navHeader/src/UserInfo.vue
@@ -0,0 +1,52 @@
+<template>
+  <el-dropdown>
+    <span class="el-dropdown-link">
+      <el-icon class="el-icon--right">
+        <Avatar />
+      </el-icon>
+      <span> {{ name }}</span>
+    </span>
+    <span></span>
+    <template #dropdown>
+      <el-dropdown-menu>
+        <el-dropdown-item
+          ><el-icon><SwitchButton /></el-icon>退出登录</el-dropdown-item
+        >
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from 'vue'
+import { IUseStore } from '@/store'
+
+export default defineComponent({
+  setup() {
+    const store = IUseStore()
+    const name = computed(() => store.state.login.userInfo.name)
+
+    return {
+      name
+    }
+  }
+})
+</script>
+<style scoped lang="less">
+.el-dropdown-link {
+  cursor: pointer;
+  //color: var(--el-color-primary);
+  display: flex;
+  align-items: center;
+
+  .el-icon--right {
+    height: 30px;
+    width: 30px;
+  }
+
+  .el-icon svg {
+    width: 1.5rem;
+    height: 1.5rem;
+  }
+}
+</style>
