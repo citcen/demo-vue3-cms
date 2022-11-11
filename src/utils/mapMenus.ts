511ae630b3937145a0dbba8445082f561db91c57
citcen <citkecen@gmail.com>
2022-11-11 14:49:46 +0800

    style: annotate

--- src/utils/mapMenus.ts
+++ src/utils/mapMenus.ts
@@ -1,6 +1,10 @@
 import type { RouteRecordRaw } from 'vue-router'
 import allRouters from '@/router/mappingRelations'
-
+/**
+ * 格式化数据 生成 vue-router 层级路由表
+ * @param menus 菜单数据
+ * @return RouteRecordRaw
+ */
 export const mapMenusToRoutes = (menus: any[]): RouteRecordRaw[] => {
   const routes: RouteRecordRaw[] = []
 
@@ -8,9 +12,11 @@ export const mapMenusToRoutes = (menus: any[]): RouteRecordRaw[] => {
   const _recurseGetRoute = (menus: any[]) => {
     for (const menu of menus) {
       if (menu.type === 2) {
+        // 子菜单
         const route = allRouters.find((route) => route.path === menu.url)
         if (route) routes.push(route)
       } else {
+        // 主菜单
         _recurseGetRoute(menu.children)
       }
     }
